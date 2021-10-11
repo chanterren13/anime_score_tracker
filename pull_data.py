a07ef0b07488ddc384396871714fb2a4ec6ec79e
Terren <terrenjchan@gmail.com>
2021-10-11 14:59:05 -0400

    add week to dataframe, select only current shows

--- pull_data.py
+++ pull_data.py
@@ -5,14 +5,16 @@ import pandas as pd
 driver = webdriver.Chrome(executable_path='/mnt/c/webdrivers/chromedriver.exe')
 driver.get('https://myanimelist.net/anime/season')
 
-total_scores = pd.DataFrame(columns=['name', 'source', 'score'])
+total_scores = pd.DataFrame(columns=['name', 'source', 'week', 'score'])
+week = 1
 
-animes = driver.find_elements_by_css_selector('.seasonal-anime')
+animes = driver.find_element_by_css_selector('.seasonal-anime-list')
+animes = animes.find_elements(By.CSS_SELECTOR, '.seasonal-anime')
 for anime in animes:
     name = anime.find_element(By.CSS_SELECTOR, '.link-title')
     score = anime.find_element(By.CSS_SELECTOR, '.score-label')
     print(name.text +': '+score.text)
-    anime_info = pd.DataFrame([[name.text, 'MAL', score.text]], columns=['name', 'source', 'score'])
+    anime_info = pd.DataFrame([[name.text, 'MAL', week, score.text]], columns=['name', 'source', 'week', 'score'])
     total_scores = total_scores.append(anime_info)
 
 total_scores = total_scores[total_scores.score != 'N/A']
