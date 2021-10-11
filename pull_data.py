24fc4caebe38f92764358b0c20aa00836d75e0cb
Terren <terrenjchan@gmail.com>
2021-10-11 01:32:42 -0400

    add score dataframe

--- pull_data.py
+++ pull_data.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 driver = webdriver.Chrome(executable_path='/mnt/c/webdrivers/chromedriver.exe')
 driver.get('https://myanimelist.net/anime/season')
+
+sort_btn = driver.find_element_by_css_selector('.seasonal-sort-order-block .sort')
 
 animes = driver.find_elements_by_css_selector('.seasonal-anime')
 animes = animes[:10]
