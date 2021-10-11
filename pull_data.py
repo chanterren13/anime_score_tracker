0d9d5525b519df44ef804c6fd21e8c1ee3170e76
Terren <terrenjchan@gmail.com>
2021-10-11 14:26:20 -0400

    add score dataframe

--- pull_data.py
+++ pull_data.py
@@ -5,11 +5,15 @@ import pandas as pd
 driver = webdriver.Chrome(executable_path='/mnt/c/webdrivers/chromedriver.exe')
 driver.get('https://myanimelist.net/anime/season')
 
-sort_btn = driver.find_element_by_css_selector('.seasonal-sort-order-block .sort')
+total_scores = pd.DataFrame(columns=['name', 'source', 'score'])
 
 animes = driver.find_elements_by_css_selector('.seasonal-anime')
-animes = animes[:10]
 for anime in animes:
+    name = anime.find_element(By.CSS_SELECTOR, '.link-title')
     score = anime.find_element(By.CSS_SELECTOR, '.score-label')
-    print(score.text)
+    print(name.text +': '+score.text)
+    anime_info = pd.DataFrame([[name.text, 'MAL', score.text]], columns=['name', 'source', 'score'])
+    total_scores = total_scores.append(anime_info)
 
+total_scores = total_scores[total_scores.score != 'N/A']
+total_scores.sort_values(by='score', inplace=True, ascending=False)
