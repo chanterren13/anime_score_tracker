303c048e2fddd320d0b922d29bad4dff97c0d4ec
Terren <terrenjchan@gmail.com>
2021-10-10 21:25:53 -0400

    add score finder

--- pull_data.py
+++ pull_data.py
@@ -1,1 +1,12 @@
-from selenium import Webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path='/mnt/c/webdrivers/chromedriver.exe')
+driver.get('https://myanimelist.net/anime/season')
+
+animes = driver.find_elements_by_css_selector('.seasonal-anime')
+animes = animes[:10]
+for anime in animes:
+    score = anime.find_element(By.CSS_SELECTOR, '.score-label')
+    print(score.text)
+
