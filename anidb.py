4721a803a5ba178ea6c419c6013ee26eb5f2ed4e
Terren <terrenjchan@gmail.com>
2021-10-17 16:20:12 -0400

    add score dataframe

--- anidb.py
+++ anidb.py
@@ -6,9 +6,13 @@ import pandas as pd
 class anidb():
     def __init__(self, driver):
         self.driver = driver
+        self.source = 'anidb'
 
     def pull_data(self, week):
         self.driver.get('https://anidb.net/anime/season')
+
+        total_scores = pd.DataFrame(columns=['name', 'source', 'week', 'score'])
+
         animes = self.driver.find_elements_by_css_selector('.g_bubble .box')
         for anime in animes:
             name = anime.find_element(By.CSS_SELECTOR, '.name-colored').text
@@ -16,8 +20,13 @@ class anidb():
                 score = anime.find_element(By.CSS_SELECTOR, '.value').text
             except NoSuchElementException:
                 score = 'N/A'
-            
             print(name + ': ' + score)
+            anime_info = pd.DataFrame([[name, self.source, week, score]], columns=['name', 'source', 'week', 'score'])
+            total_scores = total_scores.append(anime_info)
+
+        total_scores = total_scores[total_scores.score != 'N/A']
+        total_scores.sort_values(by='score', inplace=True, ascending=False)
+        return total_scores[:9]
 
 if __name__ == '__main__':
     ani = anidb()
