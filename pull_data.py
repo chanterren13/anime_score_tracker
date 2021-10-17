40d10a9d09e8a17f628add1b888500ddf4abf228
Terren <terrenjchan@gmail.com>
2021-10-17 19:14:22 -0400

    add total score dataframe and file import

--- pull_data.py
+++ pull_data.py
@@ -3,12 +3,23 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import mal as mal
 import anidb as anidb
+import os.path
+
+if os.path.isfile('scores/anime_scores.csv'):
+    total_scores = pd.read_csv('scores/anime_scores.csv')
+    week = total_scores['week'].iloc[total_scores.shape[0]-1] + 1
+else:
+    total_scores = pd.DataFrame(columns=['name', 'source', 'week', 'score'])
+    week = 1
 
 driver = webdriver.Chrome(executable_path='/mnt/c/webdrivers/chromedriver.exe')
 adb = anidb.anidb(driver)
-myanilist = mal.mal(driver)
+total_scores = total_scores.append(adb.pull_data(week))
 
-total_scores = pd.concat([myanilist.pull_data(1), adb.pull_data(1)], ignore_index=False)
+myanilist = mal.mal(driver)
+total_scores = total_scores.append(myanilist.pull_data(week))
+
 
 print(total_scores)
+
 
