54cadc4a37b75341e54d44ae26ba6f9b10b33a3e
Terren <terrenjchan@gmail.com>
2021-10-17 17:40:06 -0400

    add website objects and pull data

--- pull_data.py
+++ pull_data.py
@@ -1,21 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
+import mal as mal
+import anidb as anidb
 
 driver = webdriver.Chrome(executable_path='/mnt/c/webdrivers/chromedriver.exe')
-driver.get('https://myanimelist.net/anime/season')
+adb = anidb.anidb(driver)
+myanilist = mal.mal(driver)
 
-total_scores = pd.DataFrame(columns=['name', 'source', 'week', 'score'])
-week = 1
+total_scores = pd.concat([myanilist.pull_data(1), adb.pull_data(1)], ignore_index=False)
 
-animes = driver.find_element_by_css_selector('.seasonal-anime-list')
-animes = animes.find_elements(By.CSS_SELECTOR, '.seasonal-anime')
-for anime in animes:
-    name = anime.find_element(By.CSS_SELECTOR, '.link-title')
-    score = anime.find_element(By.CSS_SELECTOR, '.score-label')
-    print(name.text +': '+score.text)
-    anime_info = pd.DataFrame([[name.text, 'MAL', week, score.text]], columns=['name', 'source', 'week', 'score'])
-    total_scores = total_scores.append(anime_info)
+print(total_scores)
 
-total_scores = total_scores[total_scores.score != 'N/A']
-total_scores.sort_values(by='score', inplace=True, ascending=False)
